54b5b47aae0fe7789e238246fb7a21a9324b6aec
Raffaello Camoriano <raffaello.camoriano@iit.it>
2015-07-29 20:24:11 +0200

    Added plots and multiple runs for learned function norm averaging

--- experiment_1.m
+++ experiment_1.m
@@ -1,5 +1,5 @@
 setenv('LC_ALL','C');
-addpath(genpath('~/Repos/Enitor'));
+% addpath(genpath('~/Repos/Enitor'));
 
 clearAllButBP;
 close all;
@@ -16,15 +16,17 @@ saveResult  = 0;
 % ds = pumadyn(4096,4096, 32 , 'n' , 'h');
 % ds = Adult(4096,4096,'plusMinusOne');
 % ds = CTslices(6000,4096);
-ds = deepRLSsynth(200,4096);
+ds = deepRLSsynth(300,4096);
 
 
 %% Sequential residual batch KRLS, gaussian kernel
 
 map = @gaussianKernel;
 filter = @tikhonov;
-filterParGuesses = logspace(0,-5,6);
-% mapParGuesses = logspace(0,-5,11);
+filterParGuesses = logspace(2,-10,30);
+% mapParGuesses = logspace(0,-2,20);
+mapParStar = logspace(1,-2,40);
+
 % 
 % filterParStar = {1e-7}%, 1e-7};
 % mapParStar = {0.1}%, 0.10};
@@ -33,10 +35,10 @@ filterParGuesses = logspace(0,-5,6);
 % mapParStar = {0.7}%, 0.10};
 
 % filterParStar = {1e-4, 1e-3};
-mapParStar = {0.7, 0.1};
+% mapParStar = {0.7, 0.1};
 
-iterations = size(mapParStar,2);
-batchRank = 200;
+iterations = 40;
+batchRank = 300;
 
 alg = sequentialResidualKrls(map , filter , ...
                         'batchRank' , batchRank ,  ...
@@ -57,6 +59,95 @@ plot(ds.X(ds.testIdx),expKRLSSeq.result.Ypred);
 scatter(ds.X(ds.trainIdx),ds.Y(ds.trainIdx))
 legend('Ground truth','Prediction','Training examples')
 hold off
+
+%%  Norms of the coefficient vactors for different scales
+
+% cNorms = cellfun(@norm, alg.c);
+% 
+% figure
+% % hold on 
+% semilogx(mapParStar,cNorms);
+% title('Norms of the coefficient vectors for different scales')
+% xlabel('\sigma')
+% ylabel('||c||')
+
+repetitions = 10;
+cNorms = zeros(repetitions, iterations);
+bestlambdas = [];
+
+for i = 1:repetitions 
+    
+    ds = deepRLSsynth(300,4096);
+
+    alg = sequentialResidualKrls(map , filter , ...
+                            'batchRank' , batchRank ,  ...
+                            'iterations' , iterations ,  ...
+                            'mapParStar' , mapParStar ,  ...
+                            'filterParGuesses', filterParGuesses , ...
+                            'verbose' , 0);
+
+    expKRLSSeq = experiment(alg , ds , 1 , true , saveResult , '' , resdir , 0);
+    expKRLSSeq.run();
+    expKRLSSeq.result
+
+    
+    for j = 1:numel(alg.c)
+        cNorms(i,j) = alg.c{j}'*alg.Ktrain{j}*alg.c{j};
+    end
+    
+%     [cNorms ; cellfun(@norm, alg.c)];
+
+    bestlambdas = [ bestlambdas ; alg.filterParStar];
+end
+
+figure
+% hold on 
+errorbar(mapParStar,mean(cNorms), std(cNorms));
+ set(gca,'XScale','log');
+title('Norms of the coefficient vectors for different scales')
+xlabel('\sigma')
+ylabel('||f||')
+
+% hold off||c||
+
+figure
+% hold on 
+errorbar(mapParStar,mean(bestlambdas), std(bestlambdas));
+ set(gca,'XScale','log');
+title('Norms of the coefficient vectors for different scales')
+xlabel('\sigma')
+ylabel('\lambda*')
+
+
+%%
+figure
+hold on 
+% errorbar(mapParStar,mean(cNorms), std(cNorms));
+plot(mapParStar,exp(mean(log(cNorms))));
+ set(gca,'XScale','log');
+title('Norms of the coefficient vectors for different scales')
+xlabel('\sigma')
+% errorbar(mapParStar,mean(bestlambdas)*1e6, std(bestlambdas)*1e6);
+plot(mapParStar,exp(mean(log(bestlambdas))));
+ set(gca,'YScale','log');
+title('Norms of the coefficient vectors for different scales')
+xlabel('\sigma')
+legend('||f||','\lambda*')
+
+
+%%
+[Ypred,YpredStesp]  = alg.test(ds.X(ds.testIdx));
+
+figure
+%     hold on
+somma = 0;
+for i = 1: numel(YpredStesp)
+    somma = somma + YpredStesp{i}
+    plot(somma)
+    pause
+end
+% hold off
+
 
 %% Sequential residual batch Nystrom KRLS
 
